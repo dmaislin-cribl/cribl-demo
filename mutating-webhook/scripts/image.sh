23b1669f36aa4e8514ea3790ca2b4b99adf921d5
Clint Sharp <clint@cribl.io>
2020-11-24 16:21:10 -0800

    upping resources for logstream

--- mutating-webhook/scripts/image.sh
+++ mutating-webhook/scripts/image.sh
@@ -12,7 +12,7 @@ cd ${WEBHOOK_DIR}
 
 IMAGE_NAME=${WEBHOOK_APP}
 IMAGE_TAG=0.0.1
-docker build -t ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG} .
+docker build --no-cache -t ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
 # echo 'Loading ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG} in kind cluster scopetest'
 # kind load docker-image --name scopetest ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
@@ -21,5 +21,5 @@ echo 'Building image for init container'
 cd ${SCOPE_DIR}
 IMAGE_NAME=${INIT_APP}
 IMAGE_TAG=0.0.1
-docker build -t ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG} .
+docker build --no-cache -t ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${DOCKER_REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
