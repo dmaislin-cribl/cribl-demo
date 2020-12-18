b28fa8bf747643605259652a2cba7e56c32e8e38
Steve Litras <steve@cribl.io>
2020-12-18 08:41:53 -0800

    implemented new aws workergroup

---
+++ cribl/master/groups/logs/default/cribl/functions/numerify/index.js
@@ -0,0 +1,30 @@
+exports.disabled = 0;
+exports.name = 'Numerify';
+exports.version = '0.2';
+exports.group = 'Standard';
+
+let conf = {};
+let WLIgnoreList = null;
+
+exports.init = (opt) => {
+  conf = (opt || {}).conf || {};
+  WLIgnoreList = null;
+  conf.ignoreFields = (conf.ignoreFields || []).map(k => k.trim()).filter(k => k.length);
+  if (conf.ignoreFields.length > 0) {
+    WLIgnoreList = new C.util.WildcardList(conf.ignoreFields);
+  }
+};
+
+exports.process = (event) => {
+  if (!event) return event;
+
+  event.__traverseAndUpdate(5, (path, val) => {
+    const asNum = Number(val);
+    if (Number.isNaN(asNum) || (WLIgnoreList && WLIgnoreList.test(path))) {
+      return val;
+    }
+    return asNum;
+  });
+  return event;
+};
+
