cbc883e6e39afafce15b77477e4e6747d72430d2
Steve Litras <swl@litras.org>
2021-01-14 14:17:21 -0800

    Revert "K8s demochanges 2.4"

---
+++ gogen-pantraffic-syslog/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Out - ${CRIBL_PAN_SYSLOG}"
+if [ "$1" = "start" ]; then
+    gogen -v -c /etc/gogen/samples/pan.yml -o tcp --url ${CRIBL_SYSLOG} -ot rfc5424 -at -lj gen -s pan
+fi
+
+exec "$@"
